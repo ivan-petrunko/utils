7019a0a99d244e497b35c0f6f1a08fcdfc86d90e
Ivan Petrunko <ivan@petrunko.com>
2019-05-20 23:21:33 +1000

    Update mycleanup.sh
    - Add libreoffice backup cleaning
    - Add composer cache cleaning

--- mycleanup.sh
+++ mycleanup.sh
@@ -4,6 +4,8 @@ rm -f /home/$USER/.cache/thumbnails/normal/*.png > /dev/null 2>&1
 rm -f /home/$USER/.cache/mozilla/firefox/*.default/cache2/entries/* > /dev/null 2>&1
 rm -f "/home/$USER/.cache/google-chrome/Default/Cache/"* > /dev/null 2>&1
 rm -f "/home/$USER/.cache/google-chrome/Default/Media Cache/"* > /dev/null 2>&1
+rm -f "/home/$USER/.config/libreoffice/4/user/backup/"* > /dev/null 2>&1
+composer clear-cache -q > /dev/null 2>&1
 yarn cache clean --force > /dev/null 2>&1
 npm cache clean --force > /dev/null 2>&1
 docker rm $(docker ps -a -q) > /dev/null 2>&1
