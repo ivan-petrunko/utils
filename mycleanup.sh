434a8b6d890f4e17a0b466d2cd8094424389d717
Ivan Petrunko <ivan@petrunko.com>
2019-05-20 23:38:07 +1000

    Update mycleanup.sh
    - Add Google Chrome Service Worker Cache clearing

--- mycleanup.sh
+++ mycleanup.sh
@@ -5,6 +5,8 @@ rm -f /home/$USER/.cache/mozilla/firefox/*.default/cache2/entries/* > /dev/null 
 rm -f "/home/$USER/.cache/google-chrome/Default/Code Cache/js/"* > /dev/null 2>&1
 rm -f "/home/$USER/.cache/google-chrome/Default/Cache/"* > /dev/null 2>&1
 rm -f "/home/$USER/.cache/google-chrome/Default/Media Cache/"* > /dev/null 2>&1
+rm -rf "/home/$USER/.config/google-chrome/Default/Service Worker/CacheStorage/"* > /dev/null 2>&1
+rm -f "/home/$USER/.config/google-chrome/Default/Service Worker/ScriptCache/"* > /dev/null 2>&1
 rm -f "/home/$USER/.config/libreoffice/4/user/backup/"* > /dev/null 2>&1
 rm -f "/home/$USER/.local/share/Trash/files/"* > /dev/null 2>&1
 composer clear-cache -q > /dev/null 2>&1
