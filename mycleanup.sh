33b45351bfbdf715068f8fbdcf47f69ecc136e75
Ivan Petrunko <ivan@petrunko.com>
2019-05-20 23:44:41 +1000

    Update mycleanup.sh
    - Add Chromium clearing

--- mycleanup.sh
+++ mycleanup.sh
@@ -2,9 +2,12 @@
 rm -f /home/$USER/.cache/thumbnails/large/*.png > /dev/null 2>&1
 rm -f /home/$USER/.cache/thumbnails/normal/*.png > /dev/null 2>&1
 rm -f /home/$USER/.cache/mozilla/firefox/*.default/cache2/entries/* > /dev/null 2>&1
-rm -f "/home/$USER/.cache/google-chrome/Default/Code Cache/js/"* > /dev/null 2>&1
+rm -f "/home/$USER/.cache/chromium/Default/Cache/"* > /dev/null 2>&1
 rm -f "/home/$USER/.cache/google-chrome/Default/Cache/"* > /dev/null 2>&1
+rm -f "/home/$USER/.cache/google-chrome/Default/Code Cache/js/"* > /dev/null 2>&1
 rm -f "/home/$USER/.cache/google-chrome/Default/Media Cache/"* > /dev/null 2>&1
+rm -rf "/home/$USER/.config/chromium/Default/Service Worker/CacheStorage/"* > /dev/null 2>&1
+rm -f "/home/$USER/.config/chromium/Default/Service Worker/ScriptCache/"* > /dev/null 2>&1
 rm -rf "/home/$USER/.config/google-chrome/Default/Service Worker/CacheStorage/"* > /dev/null 2>&1
 rm -f "/home/$USER/.config/google-chrome/Default/Service Worker/ScriptCache/"* > /dev/null 2>&1
 rm -f "/home/$USER/.config/libreoffice/4/user/backup/"* > /dev/null 2>&1
