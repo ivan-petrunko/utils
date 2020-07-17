c27b3cceb6684f32a29168b3e63ab842576bc9ae
Ivan Petrunko <ivan@petrunko.com>
2020-07-17 14:35:02 +1000

    [backup] Update backup scripts

--- backup/backupitall.sh
+++
@@ -1,6 +0,0 @@
-# Back up it all
-#!/bin/bash
-/usr/local/bin/mybackup.sh home_config "/home/petrunko/.config"
-/usr/local/bin/mybackup.sh thunderbird "/home/petrunko/.thunderbird"
-/usr/local/bin/mybackup.sh pidgin "/home/petrunko/.purple"
-
---
+++ backup/backupvaio.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+/usr/local/bin/mybackup.sh /media/ivan/data/backup/vaio home-partial \
+    "/home/ivan/.bash_logout \
+    /home/ivan/.bash_profile \
+    /home/ivan/.config \
+    /home/ivan/.eclipse \
+    /home/ivan/eclipse-workspace \
+    /home/ivan/.gitconfig \
+    /home/ivan/.mozilla \
+    /home/ivan/.mybashrc \
+    /home/ivan/.profile \
+    /home/ivan/.purple \
+    /home/ivan/.rtorrent.rc \
+    /home/ivan/.screenrc \
+    /home/ivan/.ssh \
+    /home/ivan/.thunderbird \
+    /home/ivan/.vimrc \
+    /home/ivan/.zshrc \
+    "
+
