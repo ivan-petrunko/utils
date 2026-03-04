ba3b3d96f8b9a4869c245a693ce6e31da889e7d6
Ivan Petrunko <ivan@petrunko.com>
2026-03-04 13:40:04 +0300

    [FIX] Fix autonetworkrestart.sh

--- autonetworkrestart.sh
+++ autonetworkrestart.sh
@@ -7,10 +7,10 @@ fi
 echo Launched at $(date "+%Y-%m-%d %H:%M:%S")
 
 while true; do
-    if ! [ "`ping -c 1 -W 10 google.com`" ]; then
+    if ! [ "`ping -c 1 -W 5 google.com`" ]; then
         nmcli networking off && nmcli networking on
         echo Network restarted $(date "+%Y-%m-%d %H:%M:%S")
     fi
-    sleep 300
+    sleep 20
 done
 
